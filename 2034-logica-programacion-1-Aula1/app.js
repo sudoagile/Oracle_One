ba388bd1f8160c55055fcc2abe3f3d796b1179a4
Eduardo Ramirez De Lama <eramirez4>
2025-02-03 00:34:25 -0500

    Primero finalizado

--- 2034-logica-programacion-1-Aula1/app.js
+++ 2034-logica-programacion-1-Aula1/app.js
@@ -1,23 +1,35 @@
 //Variables
-let numeroSecreto = 5;
+let numeroMaximoPosible = 100;
+let numeroSecreto = Math.floor(Math.random()*numeroMaximoPosible)+1;
 let numeroUsuario=0;
+let intentos = 1;
+// let palabraVeces = 'vez';
+let maximoIntentos=6;
+
 
 while (numeroSecreto != numeroUsuario) {
-    numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
+    numeroUsuario = parseInt(prompt(`Me indicas un numero entre 1 y ${numeroMaximoPosible} por favor:`));
 
-    console.log(numeroUsuario);
+    console.log(typeof(numeroSecreto));
     /*
     Este código realiza
     la comparación
     */
     if (numeroUsuario == numeroSecreto) {
 
-        alert(`Acertaste, el número es: ${numeroUsuario}`);
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos}
+         ${intentos ==1 ?'vez': 'veces'}`);
     } else {
         if (numeroUsuario > numeroSecreto) {
             alert('El número secreto es menor')
         } else {
             alert('El número secreto es mayor')
+        }
+        intentos ++;
+        // palabraVeces ='veces';
+        if (intentos > 3){
+            alert(`Llegastes al número máximo de ${maximoIntentos} intentos`);
+            break;
         }
 
     }
